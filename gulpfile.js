f3d59a8e88a2c84904d30fa2e7c620a18c80762d
michae1duncan <michaelgduncan@yahoo.com>
2016-08-18 22:50:05 -0600

    added gulp processes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -13,4 +14,15 @@ gulp.task('watch', ['browserSync'], function (){
   gulp.watch('app/css/*.css', browserSync.reload);
   gulp.watch('app/*.html', browserSync.reload);
   gulp.watch('app/js/*.js', browserSync.reload);
+});
+
+gulp.task('images', function(){
+  return gulp.src('app/images/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'));
 });
